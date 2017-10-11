8c25c2b115c1d9d76cba566c5867f321d41441a0
lowerbyte <fdziki6@gmail.com>
2017-10-11 14:43:59 +0200

    Updating VM code

--- vitrualmachine.py
+++ vitrualmachine.py
@@ -2,7 +2,7 @@ import struct
 import sys
 
 def lbADD(lbvm, *args):
-    lbvm.registers[args[0]].r = lbvm.registers[args[0]].r + lbvm.registers[args[1]].r
+    lbvm.registers[args[0]].r = lbvm.registers[args[0]].r + lbvm.registers[struct.unpack("B",args[1])[0]].r
 
 def lbCMP(lbvm, *args):
     compare = lbvm.registers[args[0]].r - lbvm.registers[args[1]].r
@@ -15,12 +15,18 @@ def lbMOV(lbvm, *args):
     lbvm.registers[args[0]].r = lbvm.registers[args[1]].r
 
 def lbSET(lbvm, *args):
-    print("SET")
-    lbvm.registers[args[0]] = struct.unpack("<H", args[1])[0]
+    #print("SET")
+    lbvm.registers[args[0]].r = struct.unpack("<H", args[1])[0]
+    #print("Numer Rejestru: ", args[0])
+    #print("Wartość po SET: ", lbvm.registers[args[0]].r)
 
 def lbLB(lbvm, *args):
-    byte = lbvm.memory.loadByte(lbvm.registers[args[1]].r)
+    #print("Numer rejestru docelowego w LB: ", args[0])
+    #print("Numer rejestru początkowego w LB: ", args[1])
+    #print(struct.unpack("B",args[1])[0])
+    byte = lbvm.registers[struct.unpack("B",args[1])[0]].r
     lbvm.registers[args[0]].r = byte
+    #print("O TUTAJ" , lbvm.registers[args[0]].r)
 
 def lbSB(lbvm, *args):
     byte = lbvm.registers[args[1]].r
@@ -28,10 +34,13 @@ def lbSB(lbvm, *args):
    
 def lbOUT(lbvm, *args):
     #This won't be implemented, instead I will use simple print function.
-    print(chr(struct.unpack('B', args[0])[0]))
+    #print(hex(lbvm.memory.mem[lbvm.registers[args[0]].r]))
+    if (hex(lbvm.memory.mem[lbvm.registers[args[0]].r])) == '0xa':
+        sys.exit()
+    print(chr(lbvm.memory.mem[lbvm.registers[args[0]].r]))
 
 dir_of_opcodes = {0x00: (lbADD,2), 0x01: (lbCMP, 2), 0x02: (lbMOV, 2), 0x03: (lbSET, 3),
-                  0x04: (lbLB, 2), 0x05: (lbSB, 2), 0x10: (lbOUT, 2)}
+                  0x04: (lbLB, 2), 0x05: (lbSB, 2), 0x10: (lbOUT, 1)}
 
 class LBRegisters:
     def __init__(self):
@@ -69,14 +78,14 @@ class LBVM:
             #print(self.memory.mem)
 
     def execute(self, opcode):
-        print(self.MIP.r+1)
-        print(self.MIP.r+2, self.MIP.r+1 + dir_of_opcodes[self.MIP.r][1]+1)
+        #print(self.MIP.r+1)
+        #print(self.MIP.r+2, self.MIP.r+1 + dir_of_opcodes[self.memory.mem[self.MIP.r]][1])
         arg1 = self.memory.mem[self.MIP.r+1]
-        arg2 = self.memory.mem[self.MIP.r+2 : self.MIP.r+1 + dir_of_opcodes[self.MIP.r][1]+1]
+        arg2 = self.memory.mem[self.MIP.r+2 : self.MIP.r+1 + dir_of_opcodes[self.memory.mem[self.MIP.r]][1]]
         func = dir_of_opcodes[self.memory.mem[self.MIP.r]][0]
-        print("TEST ",func, arg1, arg2)
+        #print("TEST ",func, arg1, arg2)
         func(self, arg1, arg2)
-        print(dir_of_opcodes[self.memory.mem[self.MIP.r]][1])
+        #print(dir_of_opcodes[self.memory.mem[self.MIP.r]][1])
         self.MIP.r +=dir_of_opcodes[self.memory.mem[self.MIP.r]][1]+1
 
     
@@ -86,7 +95,8 @@ class LBVM:
 
 if __name__ == '__main__':
     m = LBVM()
-    m.readBytecode("testResult")
+    m.readBytecode("testResult2")
     m.run()
+
 
 
